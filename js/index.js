ef242dc3d6caa54d03c7894cf5d2892714d4428a
Sergey Sidoryaka <s.sidoryaka@gmail.com>
2021-10-05 00:43:10 +0300

    add skelet html

--- js/index.js
+++ js/index.js
@@ -3,29 +3,31 @@ const imgCover = new Image(200, 200);
 imgCover.src = '/img/cover.png';
 const imgN1 = new Image(200, 200);
 imgN1.src = '/img/haku.png';
-console.log(imgCover);
 
-const fliper = document.querySelector('.flipper');
+const fliper = document.querySelectorAll('.flipper');
 const cover = document.createElement('div');
 const img1 = document.createElement('div');
+const wrap = document.getElementById('wraper');
+
+console.log(fliper);
 
 cover.classList.add('card');
-cover.style.background = `url(${imgCover})`;
+cover.style.backgroundImage = `url(${imgCover.src})`;
 cover.style.backgroundSize = 'cover';
 cover.style.transition = '1s';
 
 img1.classList.add('card');
-img1.style.background = `url(${imgN1})`;
+img1.style.background = `url(${imgN1.src})`;
 img1.style.backgroundSize = 'cover';
 img1.style.transition = '1s';
 img1.style.transform = 'rotateY(180deg)';
 
-fliper.append(cover);
-fliper.append(img1);
+fliper[0].append(cover);
+fliper[0].append(img1);
 
 let flip = true;
 
-fliper.addEventListener('click', (e) => {
+fliper[0].addEventListener('click', (e) => {
   if (flip) {
     cover.style.transform = 'rotateY(180deg)';
     img1.style.transform = 'rotateY(360deg)';
